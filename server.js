982de92e325f3b717b6a9b86b6a58addb5596b90
asketsystem <asketsystem1@gmail.com>
2022-03-30 19:49:38 +0000

    POST

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 
 const app = express();
 
+// GET
 app.get("/welcome/:name", (req, res) => {
   let { name } = req.params;
   let { friends } = req.query;
@@ -10,6 +11,14 @@ app.get("/welcome/:name", (req, res) => {
       friends == "true" ? "your friends" : ""
     } to Android Innovators `
   );
+});
+
+// POST
+app.use(express.json());
+app.post("/upload/product", (req, res) => {
+  const product = req.body;
+  console.log(product);
+  res.end("product was received");
 });
 
 app.listen(8080, () => {
